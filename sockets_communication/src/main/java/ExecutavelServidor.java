261ec713466717b456931d96bf1306d7f3d2210c
Max Singular <max.starke@singular.inf.br>
2023-08-10 21:05:12 -0300

    executavel do servidor

---
+++ sockets_communication/src/main/java/ExecutavelServidor.java
@@ -0,0 +1,16 @@
+import sockets.ConexaoServidor;
+
+import java.io.IOException;
+
+public class ExecutavelServidor {
+
+    public static void main(String[] args) {
+
+        try {
+            ConexaoServidor.getInstance().getConnection(80);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+}
