54108176a2d9f65fea3cea0163794e06d878ca37
JEKrieger <joaoeduardokrieger123@gmail.com>
2023-08-11 18:23:35 -0300

    Desenvolvimento PessoaDao

--- src/DAO/PessoaDao.java
+++ src/DAO/PessoaDao.java
@@ -20,21 +20,89 @@ public class PessoaDao {
     
     public void insert(String[] mensagem){
     	
+    	String cpf = mensagem[2].split("=")[1];
+    	String nome = mensagem[3].split("=")[1];
+    	String endereco = mensagem[4].split("=")[1];
+    	
+    	Pessoa pessoa = new Pessoa(nome, cpf, endereco);
+   
+    	pessoas.put(cpf, pessoa);
 	}
     
     public String update(String[] mensagem){
-    	return "";
+    	
+    	String cpf = mensagem[2].split("=")[1];
+    	String nome = mensagem[3].split("=")[1];
+    	String endereco = mensagem[4].split("=")[1];
+    	
+    	Pessoa pessoa = pessoas.get(cpf);
+    	
+    	if(pessoa != null) {
+    		pessoa.setCpf(nome);
+    		pessoa.setEndereco(endereco);
+    		return "Pessoa atualizada com sucesso";
+    	}
+    	else {
+    		return "Pessoa não encontrada!";
+    	}
 	}
     
     public String delete(String[] mensagem){
-    	return "";
+    	
+    	String cpf = mensagem[2].split("=")[1];
+    	if(pessoas.size() > 0) {
+    		
+    		Pessoa pessoa = pessoas.get(cpf);
+    		
+    		if(pessoa != null) {
+    			pessoas.remove(cpf);
+    			return "Pessoa removida com sucesso";
+    		}
+    		else {
+        		return "Pessoa não encontrada";
+    		}
+    	}
+    	else {
+    		return "Sem pessoas cadastradas";	
+    	}
 	}
     
     public String get(String[] mensagem){
-    	return "";
+    	
+    	String cpf = mensagem[2].split("=")[1];
+    	
+    	if(pessoas.size() > 0) {
+    		
+    		Pessoa pessoa = pessoas.get(cpf);
+    		
+    		if(pessoa != null) {
+    			return pessoa.toString();
+    		}
+    		else {
+    			return "Pessoa não encontrada";
+    		}
+    	}
+    	else {
+        	return "Sem pessoas cadastradas";
+    	}
 	}
 	
     public String list(String[] mensagem){
-    	return "";
+    	
+    	String resposta = "";
+    	
+    	if(pessoas.size() > 0) {
+    		
+    		resposta += pessoas.size()+" \n";
+    		
+    		for (Map.Entry<String, Pessoa> pessoa : pessoas.entrySet()) {
+    			resposta += pessoa.getValue().getCpf()+";"+pessoa.getValue().getNome()+";"+pessoa.getValue().getEndereco()+" \n";
+    		}
+    		
+    		return resposta;
+    	}
+    	else {
+        	return "0";
+    	}
     }
 }
